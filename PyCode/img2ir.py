aa3a0ef91710ee9f0e8be625f407b251f9adc68f
DrSpaniel <nuclearrocketcar@gmail.com>
2022-12-02 16:02:25 -0500

    fixed file error
    flipper only reads .ir files with lower case s in signals

--- PyCode/img2ir.py
+++ PyCode/img2ir.py
@@ -143,7 +143,7 @@ fileName = sys.argv[1]
 
 #Output
 with open(f"{fileName}.ir", "w") as f:
-    f.write(f"""Filetype: IR Signals file
+    f.write(f"""Filetype: IR signals file
 Version: 1
 #
 name: {fileName}
